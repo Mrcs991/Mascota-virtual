245c898435fc22ebe53ed8b892a0c9cf8b619c3c
Marcos <cangamoranmarcos@gmail.com>
2022-12-29 13:01:43 +0100

    Commit#2

--- src/mascota/RatoncitoFiuFiu.java
+++ src/mascota/RatoncitoFiuFiu.java
@@ -4,17 +4,43 @@ public class RatoncitoFiuFiu {
     //
     // La clase mascota.RatoncitoFiuFiu aun no tiene ningun atributo
     //
+    private String nombre;
+    private int edad;
+    private int peso;
+    private byte hambre;
+    private byte suciedad;
+    private byte salud;
+    private byte energia;
 
-    public RatoncitoFiuFiu(String nombre, int i, int i1, int i2, int i3, int i4) {
+    public RatoncitoFiuFiu(String nombre, int peso, byte hambre, byte suciedad, byte salud, byte energia) {
         // Un objeto mascota.RatoncitoFiuFiu debería informar cuando nace...
+        this.energia = energia;
+        this.hambre = hambre;
+        this.peso = peso;
+        this.suciedad = suciedad;
+        this.salud = salud;
+        this.nombre = nombre;
+        edad = 0;
     }
+
+
+
+
+
 
 
     public String estadisticas() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Peso: ").append(this.peso);
+        sb.append("\nHambre: ").append(this.hambre);
+        sb.append("\nSuciedad: ").append(this.suciedad);
+        sb.append("\nSalud: ").append(this.salud);
+        sb.append("\nEnergia: ").append(this.energia);
+        return sb.toString();
     }
 
-    public void limpiar(int i) {
+    public void limpiar(float esfuerzoHigienico) {
+        this.suciedad -= esfuerzoHigienico;
     }
 
     public int queTramoEdad() {
@@ -44,10 +70,12 @@ public class RatoncitoFiuFiu {
         return false;
     }
 
-    public void alimentar(int b) {
+    public void alimentar(float cantidadAlimento) {
+        this.hambre -= cantidadAlimento;
 
     }
 
-    public void curar(int i) {
+    public void curar(float cantidadMedicina) {
+        this.salud += cantidadMedicina;
     }
 }
